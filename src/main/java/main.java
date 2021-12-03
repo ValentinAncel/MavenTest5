d2c427c2dfd7d079fadf87fe3a405ed8d9cabf3a
Valentin Ancel <valentin-ancel@hotmail.fr>
2021-12-03 11:33:52 +0100

    ajout de classe Class et main

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,6 @@
 public class main {
 
-    public static void main(String[] args){}
-
+    public static void main(String[] args){
+        System.out.println("Voici mon projet pour l'automatisation des tests");
+    }
 }
